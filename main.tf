0f46aeb91df99c9a7e38eb1bf21e6290e5f3a5cf
Andrew Dass <dassandrew3@gmail.com>
2022-01-17 08:37:51 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -13,10 +13,10 @@ provider "aws" {
 }
 
 
-#Test EC2
+# #Test EC2
   
-resource "aws_instance" "ubuntu" {
-  ami           = "ami-013f17f36f8b1fefb"
-  instance_type = "t2.micro"
-#   subnet_id     = ""
-}
+# resource "aws_instance" "ubuntu" {
+#   ami           = "ami-013f17f36f8b1fefb"
+#   instance_type = "t2.micro"
+# #   subnet_id     = ""
+# }
