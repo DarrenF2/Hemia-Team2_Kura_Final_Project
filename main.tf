b82e9e0312da2623571de08154500347b85b3b9f
Andrew Dass <dassandrew3@gmail.com>
2022-01-17 10:11:15 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -19,7 +19,7 @@ resource "aws_ecr_repository" "create_hemia_ecr_repo" {
 }
   
 # Create ECS Cluster
-resource "aws_ecs-cluster" "create_hemia_cluster" {
+resource "aws_ecs_cluster" "create_hemia_cluster" {
   name = "hemia-cluster"
 }
 
