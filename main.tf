d6859366a171d2d4b124d54f61838e639f196c38
Andrew Dass <dassandrew3@gmail.com>
2022-01-17 12:38:54 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -39,15 +39,14 @@ resource "aws_ecs_task_definition" "hemia_task" {
 
 
 # AWS Security Group
+module "web_server_sg" {
+  source = "terraform-aws-modules/security-group/aws//modules/http-80"
 
-resource "aws_security_group_rule" "hemia_security" {
-  type              = "ingress"
-  from_port         = 80
-  to_port           = 8081
-  protocol          = "tcp"
-  
-  ipv4_cidr_blocks  = [aws_vpc.hemia_security.ipv4_cidr_block]
-  security_group_id = "sg-123-hemia"
+  name        = "hemia-web-server"
+  description = "Security group for web-server with HTTP ports open within VPC"
+  vpc_id      = "vpc-12345678"
+
+  ingress_cidr_blocks = ["10.10.0.0/16"]
 }
 
 
